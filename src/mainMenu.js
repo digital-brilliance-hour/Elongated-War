9be78d51f75c8cdf3c9cc0e0207be7b95717ae80
josiahlaster <josiahlaster1@gmail.com>
2024-07-22 11:31:25 -0400

    finsh2

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -10,7 +10,7 @@ BasicGame.MainMenu.prototype = {
 
   create: function () {
 
-    //this.load.image('titlepage', 'assets//titleScreen1.png');
+    this.load.image('titlepage', 'assets/titleScreen1.png');
     //  We've already preloaded our assets, so let's kick right into the Main Menu itself.
     //  Here all we're doing is playing some music and adding a picture and button
     //  Naturally I expect you to do something significantly better :)
