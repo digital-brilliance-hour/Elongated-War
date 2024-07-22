a6a8821be703eacde7cdf349ec75afd7726a1e78
josiahlaster <josiahlaster1@gmail.com>
2024-07-22 05:06:49 -0400

    test final

---
+++ src/preloader.js
@@ -0,0 +1,87 @@
+
+BasicGame.Preloader = function (game) {
+
+  this.background = null;
+  this.preloadBar = null;
+
+  //this.ready = false;
+
+};
+
+BasicGame.Preloader.prototype = {
+
+  preload: function () {
+
+    //  Show the loading progress bar asset we loaded in boot.js
+    this.stage.backgroundColor = '#2d2d2d';
+
+    this.preloadBar = this.add.sprite(this.game.width / 2 - 100, this.game.height / 2, 'preloaderBar');
+    this.add.text(this.game.width / 2, this.game.height / 2 - 30, "Loading...", { font: "32px monospace", fill: "#fff" }).anchor.setTo(0.5, 0.5);
+
+    //  This sets the preloadBar sprite as a loader sprite.
+    //  What that does is automatically crop the sprite from 0 to full-width
+    //  as the files below are loaded in.
+    this.load.setPreloadSprite(this.preloadBar);
+
+    //  Here we load the rest of the assets our game needs.
+    this.load.image('titlepage', 'assets/screenImages/titleScreen1.png');
+    this.load.image('sea', 'assets/sea.png');
+    //this.load.image('sand', 'assets/sand.png');
+    this.load.image('bg1','assets/bg1.png');
+    this.load.image('first_enemy','assets/Enemies/1.png');
+    this.load.image('second_enemy','assets/Enemies/5.png');
+    this.load.image('third_enemy','assets/Enemies/2.png');
+    this.load.image('tpEnemy', 'assets/Enemies/6.png');
+    this.load.image('twEnemy', 'assets/Enemies/3.png');
+    this.load.image('tpjEnemy', 'assets/Enemies/4.png');
+    this.load.image('bg2' , 'assets/bg2.png');
+    this.load.image('plane','assets/plane.png');
+    this.load.image('bullet', 'assets/bullet.png');
+    this.load.image('enemyBullet', 'assets/enemy-bullet.png');
+    this.load.image('powerup1', 'assets/PowerUps/Powerup.png');
+    this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
+    this.load.spritesheet('whiteEnemy', 'assets/shooting-enemy.png', 32, 32);
+    this.load.image('boss', 'assets/Boss_1/0.png', 93, 75);
+    this.load.image('boss2', 'assets/Boss_2/0.png', 93, 75);
+    this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
+    this.load.spritesheet('player', 'assets/player.png', 64, 64);
+    this.load.audio('explosion', ['assets/explosion.ogg', 'assets/explosion.wav']);
+    this.load.audio('playerExplosion', ['assets/player-explosion.ogg', 'assets/player-explosion.wav']);
+    this.load.audio('enemyFire', ['assets/enemy-fire.ogg', 'assets/enemy-fire.wav']);
+    this.load.audio('playerFire', ['assets/player-fire.ogg', 'assets/player-fire.wav']);
+    this.load.audio('powerUp', ['assets/powerup.ogg', 'assets/powerup.wav']);
+    this.load.audio('titleMusic', ['assets/bgm/titlemusic.ogg']);
+    this.load.audio('stageMusic', ['assets/bgm/stageonemusic.ogg']);
+    this.load.audio('bossMusic', ['assets/bgm/bossmusic.ogg']);
+    this.load.audio('gameOverMusic', ['assets/bgm/gameovermusic.mp3']);
+    //  + lots of other required assets here
+
+  },
+
+  create: function () {
+
+    //  Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
+    this.preloadBar.cropEnabled = false;
+
+  },
+
+  update: function () {
+
+    //  You don't actually need to do this, but I find it gives a much smoother game experience.
+    //  Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
+    //  You can jump right into the menu if you want and still play the music, but you'll have a few
+    //  seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
+    //  it's best to wait for it to decode here first, then carry on.
+    
+    //  If you don't have any music in your game then put the game.state.start line into the create function and delete
+    //  the update function completely.
+    
+    //if (this.cache.isSoundDecoded('titleMusic') && this.ready == false)
+    //{
+    //  this.ready = true;
+      this.state.start('MainMenu');
+    //}
+
+  }
+
+};
