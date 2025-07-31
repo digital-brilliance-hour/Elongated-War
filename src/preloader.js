2d59437b44dfb93f4d7316340380de9115d11289
digitalbrilliancehour <48660450+digitalbrilliancehour@users.noreply.github.com>
2025-07-31 16:07:51 -0400

    Update preloader.js

--- src/preloader.js
+++ src/preloader.js
@@ -79,8 +79,9 @@ BasicGame.Preloader.prototype = {
     //if (this.cache.isSoundDecoded('titleMusic') && this.ready == false)
     //{
     //  this.ready = true;
+    if (this.input.keyboard.isDown(Phaser.Keyboard.Z) || this.input.activePointer.isDown) {
       this.state.start('MainMenu');
-    //}
+    }
 
   }
 
