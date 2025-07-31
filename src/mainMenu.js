e4d9a917406c3c50cc65ed808663abacb8531dc5
digitalbrilliancehour <48660450+digitalbrilliancehour@users.noreply.github.com>
2025-07-31 15:46:53 -0400

    Update mainMenu.js

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -11,6 +11,7 @@ BasicGame.MainMenu.prototype = {
   create: function () {
 
     this.load.image('titlepage', 'assets/ElongatedWar.png');
+    this.load.audio('titleMusic', ['assets/bgm/titlemusic.ogg']);
     //  We've already preloaded our assets, so let's kick right into the Main Menu itself.
     //  Here all we're doing is playing some music and adding a picture and button
     //  Naturally I expect you to do something significantly better :)
